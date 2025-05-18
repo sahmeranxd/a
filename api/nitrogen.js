283d950e065fc426021468e410cd0434280398aa
Şahmeranbey <ardasarsilmaz2007@gmail.com>
2025-05-18 21:27:09 +0300

    nitrogen.js

--- api/nitrogen.js
+++ api/nitrogen.js
@@ -4,8 +4,11 @@ export default function handler(req, res) {
 
   for (let i = 0; i < count; i++) {
     const code = [...Array(16)].map(() => Math.random().toString(36)[2]).join('');
-    links.push(`https://discord.com/gifts/${code}`);
+    links.push(https://discord.com/gifts/${code});
   }
 
-  res.status(200).json({ generated: links });
-      }
+  res.status(200).json({
+    generated: links,
+    telegram: '@squedmodded'
+  });
+}
